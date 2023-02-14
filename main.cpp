2312066b19d261bbca062d7c9bd323bbdc877e9b
indexedzoom <100958431+indexedzoom@users.noreply.github.com>
2023-02-14 18:56:41 +0000

    Added first set of files.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+
+	std::cout << "\n";
+	
+	for (int num = 1; num < argc; num++) {
+		std::cout << num << ": " << argv[num] << "\n";
+	}
+
+	std::cout << "\n<------------>\n";
+	std::cout << argc - 1 << " lines\n";
+	std::cout << "<------------>\n\n";
+
+	return 0;
+}
